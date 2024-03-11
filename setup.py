04e782d19bd0d276d35f5f619509fcb8b2b01265
pupCrypto <ujif_cryptoriders@mail.ru>
2024-03-11 18:51:56 +0600

    add dep

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='google_spreadsheets',
     version='0.1.0',
-    author='PupCrypto',
-    author_email='ujif_cryptoriders@mail.ru',
+    packages=find_packages(),
+    install_requires=[
+        'requests==2.31.0',
+        'oauth2client==4.1.3',
+        'google-api-python-client==2.104.0',
+        'httplib2==0.22.0',
+        'pyasn1',
+    ]
 )
